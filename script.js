85942e97aa99e6a9534abc1ee69cbba40a105156
Anna Kazantseva <annetastara@gmail.com>
2019-10-20 22:43:53 +0300

    Solution

---
+++ script.js
@@ -0,0 +1,21 @@
+let safeCall = function(f) {
+    try {
+        f()
+    } catch (error) {
+        return false;
+    }
+    return true;
+};
+
+
+
+
+
+
+
+
+
+safeCall(() => console.log('Hello!')); // true
+safeCall(() => JSON.parse('abc')); // false
+safeCall(() => false); // true
+safeCall(() => abc); // false
